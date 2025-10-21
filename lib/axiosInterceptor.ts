560868def465df29840374df7e8ec8d3859aaffc
SahilPawar10 <pawarsp959@gmail.com>
2025-10-22 01:31:41 +0530

    backend live url added

--- lib/axiosInterceptor.ts
+++ lib/axiosInterceptor.ts
@@ -9,7 +9,8 @@ interface JwtPayload {
 }
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/v1", // local server
+  baseURL: "https://akmotors-server.onrender.com/v1", // deploy
+  // baseURL: "http://localhost:5000/v1", // local server
 });
 
 axiosInstance.interceptors.request.use(
